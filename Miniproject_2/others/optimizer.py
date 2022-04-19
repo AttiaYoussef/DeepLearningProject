e2e5c8a4e249d37eb2019022ccbf9939271366c9
AttiaYoussef <youssef.attia@epfl.ch>
2022-04-19 16:08:18 +0200

    Skeleton project 2

--- Miniproject_2/others/optimizer.py
+++ Miniproject_2/others/optimizer.py
@@ -3,4 +3,7 @@ torch.set_grad_enabled(False)
 
 
 class SGD(object):
-    pass
+    def __init__(self, model_params, lr, momentum = 0):
+        self.model_params = model_params
+        self.lr = lr
+        self.momentum = momentum
