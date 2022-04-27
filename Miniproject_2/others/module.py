f66dc23a9a1e6a45425c481c81bee69cae35bb58
AttiaYoussef <youssef.attia@epfl.ch>
2022-04-27 17:41:19 +0200

    Add some code

--- Miniproject_2/others/module.py
+++ Miniproject_2/others/module.py
@@ -1,4 +1,8 @@
 import torch
+from torch import empty, cat, arange
+from torch.nn.functional import fold, unfold
+import math
+import random
 torch.set_grad_enabled(False)
 
 class Module(object):
@@ -10,16 +14,26 @@ class Module(object):
         return []
 
 class Conv2d(Module):
-    def __init__(self, in_channels, out_channels, kernel, stride):
+    
+    def __init__(self, in_channels, out_channels, kernel, stride,padding,dilation):
         super().__init__()
+        
+        def __parameter_int_or_tuple__(parameter):
+            if type(parameter) is int:
+                returned = (parameter,parameter)
+            else:
+                returned = parameter
+            return returned
+        
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.kernel = kernel
-        self.stride = stride
-        if type(kernel) is int:
-            self.window = torch.empty((kernel, kernel))
-        else:
-            self.window = torch.empty(kernel)
+        self.kernel = __parameter_int_or_tuple__(kernel)
+        self.stride = __parameter_int_or_tuple__(stride)
+        self.padding = __parameter_int_or_tuple__(padding)
+        self.dilation = __parameter_int_or_tuple__(dilation)
+        
+        self.weights = torch.empty(out_channels,self.kernel[0], self.kernel[1])
+        self.bias = torch.empty(out_channels)
             
     def forward(self, x):
         pass
